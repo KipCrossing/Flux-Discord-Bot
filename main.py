153e28524ee2ed120ee303ed9651a9827169f621
KipCrossing <kip.crossing@gmail.com>
2019-03-04 21:27:12 +1100

    !IBDD and allocate roles

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ client = commands.Bot(command_prefix = '!')
 
 @client.event
 async def on_ready():
+    await client.change_presence(game = discord.Game(name='type !IBDD'))
     print('Bot ready!')
 
 
@@ -16,10 +17,17 @@ async def on_message(message):
     print("New message")
     await client.process_commands(message)
 
+#write message
 @client.command()
-async def ping():
-    await client.say('Pong!!!')
+async def IBDD(*args):
+    if len(args) == 0:
+        await client.say('**To create an IBDD issue to be voted on type: ** \n !IBDD "Issue to be voted on"')
+    elif len(args) == 1:
+        await client.say('**Vote: **' + str(args[0])+'\n:ballot_box_with_check: YES \n:x: NO \n:gem: Convert vote to Political Capital')
+    else:
+        await client.say('*error*')
 
+#Read and write
 @client.command()
 async def echo(*args):
     output = ''
@@ -29,11 +37,7 @@ async def echo(*args):
     await client.say(output)
 
 
-
-@client.command()
-async def hi():
-    await client.say('Hello!')
-
+#Delete messages
 @client.command(pass_context = True)
 async def clear(ctx, amount = 100):
     channel = ctx.message.channel
@@ -43,5 +47,16 @@ async def clear(ctx, amount = 100):
         messages.append(message)
     await client.delete_messages(messages)
     await client.say('Messages deleted')
+
+
+#assign Roles
+@client.event
+async def on_member_join(member):
+    role = doscord.utils.get(member.server.roles, name = 'Cool People')
+    await client.add_roles(member,role)
+
+
+
+
 
 client.run(TOKEN)
