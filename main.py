bad650c7913842ba3943b2670416d524f9a7cfad
KipCrossing <kip.crossing@gmail.com>
2019-03-05 20:58:50 +1100

    Bot can auto-react

--- main.py
+++ main.py
@@ -1,20 +1,39 @@
 print("Flux Discord Bot")
 import discord
 from discord.ext import commands
+import asyncio
+from itertools import cycle
+
 
 TOKEN = 'SECRETE!'
 
 client = commands.Bot(command_prefix = '!')
 
+status = ['Have fun!','VOTE FLUX','Type: !IBDD']
+
+async def change_status():
+    await client.wait_until_ready()
+    msgs = cycle(status)
+    while not client.is_closed:
+        current_status = next(msgs)
+        await client.change_presence(game = discord.Game(name=current_status))
+        await asyncio.sleep(10)
+
 @client.event
 async def on_ready():
-    await client.change_presence(game = discord.Game(name='type !IBDD'))
+    await client.change_presence(game = discord.Game(name='Type: !IBDD'))
     print('Bot ready!')
 
+
+#unicode form http://www.fileformat.info/info/unicode/char/274e/index.htm
+ibdd_emojis = ['\u2611','\u274E','\U0001F48E','\U0001F4CA']
 
 @client.event
 async def on_message(message):
     print("New message")
+    for emoji in ibdd_emojis:
+        await client.add_reaction(message, emoji)
+
     await client.process_commands(message)
 
 #write message
@@ -23,7 +42,7 @@ async def IBDD(*args):
     if len(args) == 0:
         await client.say('**To create an IBDD issue to be voted on type: ** \n !IBDD "Issue to be voted on"')
     elif len(args) == 1:
-        await client.say('**Vote: **' + str(args[0])+'\n:ballot_box_with_check: YES \n:x: NO \n:gem: Convert vote to Political Capital')
+        await client.say('**Vote: **' + str(args[0])+'\n:ballot_box_with_check: YES \n:negative_squared_cross_mark: NO \n:gem: Convert vote to Political Capital \n:bar_chart:  Trade Political Capital for share in vote')
     else:
         await client.say('*error*')
 
@@ -55,8 +74,9 @@ async def on_member_join(member):
     role = doscord.utils.get(member.server.roles, name = 'Cool People')
     await client.add_roles(member,role)
 
+#Background task
 
 
 
-
+client.loop.create_task(change_status())
 client.run(TOKEN)
