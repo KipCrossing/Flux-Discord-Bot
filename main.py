9fc9abf27750e6aec6f08f5ab7b15ad320e905d5
KipCrossing <kip.crossing@gmail.com>
2019-03-19 22:55:49 +1100

    Send Poll as PM

--- main.py
+++ main.py
@@ -36,17 +36,26 @@ async def on_message(message):
         for emoji in ibdd_emojis:
             await client.add_reaction(message, emoji)
 
+
     await client.process_commands(message)
 
 #write message
-@client.command()
-async def IBDD(*args):
+@client.command(pass_context = True)
+async def IBDD(ctx, *args):
+    server_id = ctx.message.server.id
+    server = client.get_server(id = server_id)
     if len(args) == 0:
         await client.say('**To create an IBDD issue to be voted on type: ** \n !IBDD "Issue to be voted on"')
     elif len(args) == 1:
-        await client.say('**Vote: **' + str(args[0])+'\n:ballot_box_with_check: YES \n:negative_squared_cross_mark: NO \n:gem: Convert vote to Political Capital \n:bar_chart:  Trade Political Capital for share in vote')
+        if server:
+            for member in server.members:
+                if 'Flux Bot#8753' != str(member):
+                    print('name: {}'.format(member))
+                    await client.send_message(member,'**Vote: **' + str(args[0])+'\n:ballot_box_with_check: YES \n:negative_squared_cross_mark: NO \n:gem: Convert vote to Political Capital \n:bar_chart:  Trade Political Capital for share in vote')
+        await client.say('**Vote for** *{}* **will end in 5 mins**'.format(args[0]))
     else:
         await client.say('*error*')
+
 
 #Read and write
 @client.command()
@@ -85,6 +94,7 @@ async def on_reaction_add(reaction, user):
         print(reaction.emoji, user.name)
         if reaction.emoji == ibdd_emojis[0]:
             await client.send_message(user, 'You have voted **YES** {} to the issue: *{}*'.format( reaction.emoji, reaction.message.content.split('\n')[0][10:]))
+            print(user)
         elif reaction.emoji == ibdd_emojis[1]:
             await client.send_message(user, 'You have voted **NO** {} to the issue: *{}*'.format( reaction.emoji, reaction.message.content.split('\n')[0][10:]))
         elif reaction.emoji == ibdd_emojis[2]:
